72f8933240a12126acf98c425f1c36db77a3609f
DevvObiero <obieropauldev@gmail.com>
2025-03-13 13:45:54 +0300

    added admin.css and for the admin pafe

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order -Hompage</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/admin.css">  <!-- linking to our CSS stylesheet -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ballet:opsz@16..72&family=Bokor&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- menu section starting heheee(michael Jackson) -->
+ <div class="menu">
+   <div class="wrapper">
+   
+   <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Admin</a></li>
+      <li><a href="">Category</a></li>
+      <li><a href="">Food</a></li>
+      <li><a href="">Order</a></li>
+   </ul>
+   </div>
+ </div>
+<!-- menu section ending heheee(was funnier in my head) -->
+
+<!-- main content section starts here -->
+ <div class="main-content">
+ <div class="wrapper">
+ Main content Goes Here
+   </div>
+  
+ </div>
+ <!-- main content section ends here -->
+
+ <!-- footer section starts  -->
+  <div class="footer">
+  <div class="wrapper">
+<P class="text-center">
+   &copy; 2022 Food Order. All Rights Reserved. Designed by <a href="#">Your Name</a>   </P>
+<P>
+</P>
+   </div>
+   
+    </div>
+  <!-- footer section ends  -->
+</body>
+</html>
