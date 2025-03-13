5e808064bb365a73d3dcfa643ad250df052a41d6
DevvObiero <obieropauldev@gmail.com>
2025-03-13 15:15:06 +0300

    reusable code

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+
+ <!-- footer section starts  -->
+ <div class="footer">
+  <div class="wrapper">
+<P class="text-center">
+   &copy; 2022 Food Order. All Rights Reserved. Designed by <a href="https://www.instagram.com/obinazo/">Paul Obiero</a>   </P>
+<P>
+</P>
+   </div>
+   
+    </div>
+  <!-- footer section ends  -->
+</body>
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,15 @@
+<?php
+include('partials/menu.php')
+?>
+<!-- main content section starts here -->
+ <div class="main-content">
+ <div class="wrapper">
+
+ <h1 class="text-center">Manage Admin</h1>
+
+ <!-- main content section ends here -->
+
+ <?php
+
+include('./footer.php')
+?>
